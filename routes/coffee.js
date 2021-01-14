e07690611fd1b189ea6b31f893b1a28196ca90aa
Artem <artem-karganyan@bk.ru>
2021-01-14 15:32:21 +0300

    werfg

--- routes/coffee.js
+++ routes/coffee.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const Coffee = require('../models/cofee');
 
 const router = express.Router();
 
 // страница с кофе
 // ! добавить базу
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const coffes = await Coffee.find();
+  console.log(coffes);
   res.render('coffee', {
     title: 'BRO.WE.COFFE',
     isCoffe: true,
