523ba2769eff6c5c24070248f5e09dd30ed0cc38
Yuriy <uradjachkov@gmail.com>
2021-01-14 12:21:00 +0300

    Немного стилизации

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,12 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    'airbnb-base',
   ],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
   rules: {
   },
 };
--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
+const hbs = require('hbs');
+const coffeRouter = require('./routes/coffe');
 const usersRouter = require('./routes/users');
 
-const app = express();
-
+const app = express();                        
+                                         
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -19,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+app.use('/', coffeRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
