f82996023469f18b10287a3967d31165632d11db
Artem <artem-karganyan@bk.ru>
2021-01-14 15:53:23 +0300

    put db in routes cof and bak for render

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  // store: new MongoStore({ mongooseConnection: mongoose.connection }), // ! прикрепить базу
+  store: new MongoStore({ mongooseConnection: mongoose.connection }), // ! прикрепить базу
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
--- routes/coffee.js
+++ routes/coffee.js
@@ -1,19 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const Coffee = require('../models/cofee');
-
-mongoose.connect('process.env.DB_URL', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const router = express.Router();
 
-// страница с кофе
-// ! добавить базу
 router.get('/', async (req, res) => {
-  const coffes = await Coffee.find();
-  console.log(coffes);
+  const coffes = await Coffee.find({ visible: true });
   res.render('coffee', {
     title: 'BRO.WE.COFFE',
     isCoffe: true,
+    coffes,
   });
 });
 
