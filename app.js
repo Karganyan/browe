b1912c83420b572e9c7575230b43b3b4c8a8ef84
Yuriy <uradjachkov@gmail.com>
2021-01-14 15:02:26 +0300

    бульма

--- app.js
+++ app.js
@@ -51,22 +51,22 @@ app.use('/events', eventsRouter);
 app.use('/bakery', bakeryRouter);
 app.use('/auth', authRouter);
 app.use('/private', privateRouter);
-app.use('/admin', adminRouter);
+app.use('/admin', adminRouter); 
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
+// error handler  
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+ 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
+});     
+    
 module.exports = app;
