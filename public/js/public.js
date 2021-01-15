40c6aff4b6c258be35dc75a86ba758206cf1b4e1
Yuriy <uradjachkov@gmail.com>
2021-01-15 13:56:10 +0300

    styles

--- public/js/public.js
+++ public/js/public.js
@@ -21,3 +21,12 @@ body.addEventListener('click', async (e) => {
     }
   }
 });
+
+(function() {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('#'+burger.dataset.target);
+  burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+  });
+})();
